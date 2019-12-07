ccb2245fbf41886c31fcfd678dd5adb446d29600
Anchit Gupta <guptaanchit29@gmail.com>
2019-12-07 22:56:07 +0530

    dictionary of unique ids

--- avs.py
+++ avs.py
@@ -20,12 +20,7 @@ parser.add_argument('--file', help='CSV file containing data', required=True)
 parser.add_argument("--output_dir", help="Folder where final videos will be downloaded", required=True)
 args = parser.parse_args()
 
-data = []
-
-count1 = collections.Counter()
-count2 = collections.Counter()
-exts = collections.Counter()
-downloading_start = collections.Counter()
+data = {}
 # def my_hook(d):
 # 	print(d)
 # 	if d['status'] == 'finished':
@@ -60,7 +55,7 @@ def format_time(t):
 def yt_download(d):
 
 	# youtube id, start time, x, y
-	yid, start, end, x, y =  d
+	yid = d[0]
 	url = 'https://www.youtube.com/watch?v='+yid
 	path_dir = path.join(args.output_dir, yid)
 	
@@ -72,35 +67,27 @@ def yt_download(d):
 			check = None
 	
 	if check != None:
-		if not downloading_start[yid]:
-			print("Start Downloading ",yid)
-			with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		print("Start Downloading ",yid)
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+			try:
+				downloading_start[yid] = True
+				info_dict = ydl.extract_info(url, download=True)
+				ext = "." + info_dict['ext']
 				try:
-					downloading_start[yid] = True
-					info_dict = ydl.extract_info(url, download=True)
-					exts[yid] = "." + info_dict['ext']
-					try:
-						os.mkdir(path_dir)
-					except OSError:
-						print ("Creation of the directory %s failed" % path)
-				except:
-					info_dict = None
-					exts[yid] = None
-	
-		while not path.exists(path_dir):
-			time.sleep(1)
-		st = format_time(float(start))
-		et = format_time(float(end))
-		ext = exts[yid]
-		command = 'ffmpeg -hide_banner -loglevel panic -ss {} -to {} -i {} -vcodec copy -acodec copy {}'.format(st,
-						 et, path_dir + ext, path.join(args.output_dir,yid,yid+"_"+start+"_"+end+ext))
+					os.mkdir(path_dir)
+				except OSError:
+					print ("Creation of the directory %s failed" % path)
+			except:
+				info_dict = None
+				exts[yid] = None
+		if info_dict != None:
+			for start, end, x, y in d[1]:
+				st = format_time(float(start))
+				et = format_time(float(end))
+				command = 'ffmpeg -hide_banner -loglevel panic -ss {} -to {} -i {} -vcodec copy -acodec copy {}'.format(st,
+								 et, path_dir + ext, path.join(args.output_dir,yid,yid+"_"+start+"_"+end+ext))
 
-		subprocess.call(command, shell=True)
-	else:
-		print('Video {} was already downloaded'.format(yid))
-
-	count2[yid] += 1
-	if count2[yid] == count1[yid]:
+				subprocess.call(command, shell=True)
 		try:
 			os.remove(path_dir + ext)
 		except:
@@ -112,17 +99,19 @@ def preprocess():
 		csv_data = csv.reader(f,delimiter=",")
 
 		for row in csv_data:
-			data.append(row)
 			yid = row[0]
-			count1[yid] += 1
+			if yid in data:
+				data[yid].append(row[1:])
+			else:
+				data[yid] = []
 
-	if path.isdir(args.output_dir):
+	if not path.isdir(args.output_dir):
 		os.mkdir(args.output_dir)
 
 def main():
 	preprocess()
 	p = ThreadPoolExecutor(args.jobs)
-	threads = [p.submit(yt_download,row) for row in data]
+	threads = [p.submit(yt_download,row) for row in data.items()]
 	_ = [r.result() for r in tqdm(as_completed(threads), total=len(threads))]
 
 if __name__ == '__main__':
